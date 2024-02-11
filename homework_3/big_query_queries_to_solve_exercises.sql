4da807c04b203ae099b5a7e22dcecfbaae953df7
angiegf <50264468+angiegf@users.noreply.github.com>
2024-02-11 23:41:34 +0000

    Update big_query_queries_to_solve_exercises.sql
    add the location count

--- homework_3/big_query_queries_to_solve_exercises.sql
+++ homework_3/big_query_queries_to_solve_exercises.sql
@@ -1,5 +1,7 @@
 SELECT * FROM `watchful-augury-411213.ny_taxi.green_taxi_2022`;
 
+SELECT count(distinct PULocationID) as distinct_id_count FROM `watchful-augury-411213.ny_taxi.green_taxi_2022`;
+
 SELECT count(1) as nr_rows FROM `watchful-augury-411213.ny_taxi.green_taxi_2022`;
 
 SELECT count (1) as nr_records
