1db9118f2ac07618d217174270892163c8a583fb
angiegf <50264468+angiegf@users.noreply.github.com>
2024-02-18 17:59:42 +0000

    edited how to run the model without the limit (#5)

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -44,7 +44,7 @@ from tripdata
 where rn = 1
 
 
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+-- dbt build --select fact_trips --vars '{is_test_run: false}'
 {% if var('is_test_run', default=true) %} --if there's no value for is_test_run then use TRUE and by default add a limit 100, to get cheaper queries, great for production, 
 --dbt build --select <model_name> --vars '{'is_test_run': 'false'}' to deploy
 
